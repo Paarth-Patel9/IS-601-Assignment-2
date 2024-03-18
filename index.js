8bcf98e9a71914fe17d2fe56b7b4e7ad7b0eeb2d
Paarth Patel <127326715+Paarth-Patel9@users.noreply.github.com>
2024-03-19 03:15:39 +0800

    Trim update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var vowels = 0
 var cons = 0
 var input = prompt("Enter a string:")
-input = input.toLocaleLowerCase()
+input = input.toLocaleLowerCase().trim()
 
 for(var i = 0; i < input.length; i++) {
     if(input[i] === 'a'|| input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'u' || input[i] === 'a')
