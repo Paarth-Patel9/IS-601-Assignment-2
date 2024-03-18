f21266d4d2a3e899111a6b7a1245a48ed24c2e04
Paarth Patel <127326715+Paarth-Patel9@users.noreply.github.com>
2024-03-18 10:50:39 +0800

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+var vowels = 0
+var cons = 0
+var input = prompt("Enter a string:")
+input = input.toLocaleLowerCase()
+
+for(var i = 0; i < input.length; i++) {
+    if(input[i] === 'a'|| input[i] === 'e' || input[i] === 'i' || input[i] === 'o' || input[i] === 'u' || input[i] === 'a')
+        count += 1
+    if(input[i] === 'b' || input[i] === 'c' || input[i] === 'd' || input[i] === 'f' || input[i] === 'g' || input[i] === 'j' || input[i] === 'k' || input[i] === 'l' 
+    || input[i] === 'm' || input[i] === 'n' || input[i] === 'p' || input[i] === 'q' || input[i] === 's' || input[i] === 't' || input[i] === 'v'
+    || input[i] === 'x' || input[i] === 'z' || input[i] === 'h' || input[i] === 'r' || input[i] === 'w' || input[i] === 'y')
+        cons += 1
+}
+
+window.alert("Number of vowels are " + vowels + "\n" + "Number of consonants are " + cons)
